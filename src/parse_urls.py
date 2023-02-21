03e8b8e0ca63394c851b8d87b381634b275bc21b
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-02-21 11:46:11 +0100

    fix memory issue with json

--- src/parse_urls.py
+++ src/parse_urls.py
@@ -1,13 +1,14 @@
-import json
 import os
-import duckdb
 
+import duckdb
+import pyarrow
+import pyarrow.parquet as pq
+from rich.progress import track
 from ural import get_domain_name as ural_get_domain_name
 from ural import normalize_url as ural_normalize_url
 
 from CONSTANTS import LINKSTABLENAME, MAINTABLENAME, PREPROCESSDIR
 from utils import Timer
-from rich.progress import track
 
 
 def parse_urls(connection):
@@ -26,68 +27,87 @@ def parse_urls(connection):
         ) AS s
     ) tbl(link_list, tweet_id)
     """
-    links_list = duckdb.sql(query=query, connection=connection)
-    link_aggregates = links_list.aggregate('link, STRING_AGG(tweet_id)').fetchall()
+    links_list = duckdb.sql(query=query, connection=connection).fetchall()
     timer.stop()
 
-    # Parse the extracted URLs
     timer = Timer('Parsing extracted links with URAL')
-    normalized_links = {}
-    for i in track(link_aggregates):
-        raw_url = str(i[0])
-        tweet_id = [int(id) for id in i[1].split(',')]
+    # Create empty column arrays
+    link_list, tweet_ids_list, normalized_url_list, domain_name_list = [], [], [], []
+    for tuple in track(links_list):
+        # Parse the row data
+        raw_url = str(tuple[0])
         norm_url = ural_normalize_url(raw_url)
-        domain_name = ural_get_domain_name(norm_url)
-        if not normalized_links.get(norm_url):
-            normalized_links[norm_url] = {
-                'normalized_url':norm_url,
-                'domain_name':domain_name,
-                'links':[raw_url],
-                'tweet_ids':tweet_id
-            }
-        else:
-            normalized_links[norm_url]['links'].append(raw_url)
-            normalized_links[norm_url]['tweet_ids'].extend(tweet_id)
-        normalized_links[norm_url]['links'] = list(set(normalized_links[norm_url]['links']))
+        domain = ural_get_domain_name(norm_url)
+        tweet_id = int(tuple[1])
+        # Add row data to the column's array
+        link_list.append(raw_url)
+        tweet_ids_list.append(tweet_id)
+        normalized_url_list.append(norm_url)
+        domain_name_list.append(domain)
+    # Name columns
+    table_column_names = [
+        'normalized_url',
+        'domain_name',
+        'link',
+        'tweet_id',
+    ]
+    # Create Arrow table
+    timer.stop()
+    timer = Timer('Writing parsed URL data to parquet file')
+    aggregated_links_table = pyarrow.table(
+        [
+            pyarrow.array(normalized_url_list, type=pyarrow.string()),
+            pyarrow.array(domain_name_list, type=pyarrow.string()),
+            pyarrow.array(link_list, type=pyarrow.string()),
+            pyarrow.array(tweet_ids_list, type=pyarrow.int64()),
+        ],
+        names=table_column_names
+    )
 
-    # Write the parsed URL data to a JSON
-    normalized_links_json = os.path.join(PREPROCESSDIR, 'normalized_links.json')
-    nested = [row for row in normalized_links.values()]
-    with open(normalized_links_json, 'w') as fp:
-        json.dump(nested, fp, indent=4)
+    # Write table to a parquet file
+    parquet_file = os.path.join(PREPROCESSDIR, 'aggregate.parquet')
+    pq.write_table(
+        aggregated_links_table,
+        parquet_file,
+        compression='gzip'
+    )
     timer.stop()
 
-    # Aggregate links by normalized URL
-    timer = Timer('Import parsed URLs to links table')
-    json_keys_as_columns = {
+    timer = Timer('Importing parquet file to database')
+    # Read parquet file to DuckDB table
+    links_column_dict = {
         'id':'VARCHAR PRIMARY KEY',
         'normalized_url':'VARCHAR',
         'domain_name':'VARCHAR',
         'domain_id':'VARCHAR',
         'tweet_ids':'BIGINT[]',
-        'nb_tweets':'INTEGER',
-        'links':'VARCHAR[]'
+        'distinct_links':'VARCHAR[]',
     }
+    links_columns_string = ', '.join(f'{k} {v}' for k,v in links_column_dict.items())
     connection.execute(f"""
-    CREATE TABLE {LINKSTABLENAME}({', '.join([f'{k} {v}' for k,v in json_keys_as_columns.items()])})
+    CREATE TABLE {LINKSTABLENAME}({links_columns_string});
     """)
     connection.execute(f"""
     INSERT INTO {LINKSTABLENAME}
-    SELECT  md5(record.normalized_url),
-            record.normalized_url,
-            record.domain_name,
-            md5(record.domain_name),
-            record.tweet_ids,
-            len(record.tweet_ids),
-            record.links
-    FROM (
-        SELECT UNNEST(json) AS record
-        FROM (
-            SELECT *
-            FROM read_json_auto('{normalized_links_json}', maximum_depth=-1)
-        ) json
-    )
+    SELECT  md5(normalized_url),
+            normalized_url,
+            ANY_VALUE(domain_name),
+            md5(ANY_VALUE(domain_name)),
+            ARRAY_AGG(tweet_id),
+            ARRAY_AGG(DISTINCT link),
+    FROM read_parquet('{parquet_file}')
+    GROUP BY normalized_url
     """)
-    # Clean up the JSON serlialization of the parsed URL data
-    os.remove(normalized_links_json)
+    timer.stop()
+    timer = Timer('Counting sums in aggregated links table')
+    print('Counting number of tweets per link')
+    connection.execute(f"""
+    ALTER TABLE {LINKSTABLENAME} ADD COLUMN nb_tweets INTEGER DEFAULT 0;
+    UPDATE {LINKSTABLENAME} SET nb_tweets = LEN(tweet_ids);
+    """)
+    print('Counting number of link variations')
+    connection.execute(f"""
+    ALTER TABLE {LINKSTABLENAME} ADD COLUMN nb_distinct_links INTEGER DEFAULT 0;
+    UPDATE {LINKSTABLENAME} SET nb_distinct_links = LEN(distinct_links);
+    """)
     timer.stop()
