ae7055cae41456d9e353a5ff84525155c66748fa
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-02-24 18:13:41 +0100

    move histogram script outside main workflow

--- src/aggregate.py
+++ src/aggregate.py
@@ -1,9 +1,6 @@
-import ast
 import os
-import re
 
 import duckdb
-import matplotlib.pyplot as plt
 
 from CONSTANTS import AGGREGATEDDOMAINSTABLE, LINKSTABLENAME, MAINTABLENAME
 from utils import Timer
@@ -107,34 +104,4 @@ def domains(connection):
         sep=',',
         header=True,
     )
-    timer.stop()
-
-    timer = Timer('Writing histograms for high-volume domains')
-    data = duckdb.query(f"""
-    SELECT histogram_of_tweets_per_month, domain 
-    FROM {AGGREGATEDDOMAINSTABLE}
-    """, connection=connection).fetchall()
-    max_per_month = duckdb.table(
-        table_name=AGGREGATEDDOMAINSTABLE,
-        connection=connection).max('nb_tweets_per_month_with_domain').fetchone()[0]
-    histogram_dir = os.path.join('output', 'histograms')
-    os.makedirs(histogram_dir, exist_ok=True)
-    for tuple in data:
-        domain = tuple[1]
-        histogram = str(tuple[0]).replace('=',':')
-        histogram = re.sub(
-                        pattern=r'(\d{4}-\d{2}-\d{2})', 
-                        repl='"\\1"', 
-                        string=histogram
-                    )
-        dictionary = ast.literal_eval(histogram)
-        if len(list(dictionary.keys())) > 6 and sum(list(dictionary.values())) > 2000:
-            plt.title(f"Tweets per month for {domain}")
-            # x = [datetime.strptime(k, '%Y-%m-%d') for k in list(dictionary.keys())]
-            x = list(dictionary.keys())
-            y = dictionary.values()
-            plt.bar(x, y, color='g')
-            plt.ylim(-2, max_per_month+10)
-            outfile_path = os.path.join(histogram_dir, f'{domain}.png')
-            plt.savefig(outfile_path)
     timer.stop()
